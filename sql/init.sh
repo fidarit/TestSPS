6a3e89c3a267079bef577ab50f7a3367c98844da
Fidarit Mullayanov <fidarit.mullayanov@yandex.ru>
2024-10-19 20:27:03 +0500

    db: Первичное заполнение БД (данные сгенерированы gpt)

--- sql/init.sh
+++ sql/init.sh
@@ -10,8 +10,20 @@ DB_EXISTS=$(/opt/mssql-tools18/bin/sqlcmd -S localhost -U "SA" -P "$MSSQL_SA_PAS
 if ! echo "$DB_EXISTS" | grep -q "1"; then
     /opt/mssql-tools18/bin/sqlcmd -S localhost -U "SA" -P "$MSSQL_SA_PASSWORD" -C -Q "CREATE DATABASE serverdb"
 
-    for script in ./*.sql; 
+    for script in /scripts/Table_*.sql; 
     do 
         /opt/mssql-tools18/bin/sqlcmd -S localhost -U "SA" -P "$MSSQL_SA_PASSWORD" -C -d serverdb -i "$script"
     done
+
+    for script in /scripts/Trigger_*.sql; 
+    do 
+        /opt/mssql-tools18/bin/sqlcmd -S localhost -U "SA" -P "$MSSQL_SA_PASSWORD" -C -d serverdb -i "$script"
+    done
+
+    for script in /scripts/Procedure_*.sql; 
+    do 
+        /opt/mssql-tools18/bin/sqlcmd -S localhost -U "SA" -P "$MSSQL_SA_PASSWORD" -C -d serverdb -i "$script"
+    done
+    
+    /opt/mssql-tools18/bin/sqlcmd -S localhost -U "SA" -P "$MSSQL_SA_PASSWORD" -C -d serverdb -i "/scripts/Seed.sql"
 fi
